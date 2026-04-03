c78482ab92eeb5a794514829246f95163a626293
noble_kiwi <iwkr1208@gmail.com>
2026-04-03 16:05:59 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -26,38 +26,39 @@ int countPairs2(int *arr, int len, int value){
     return count;
 }
 int countPairs3(int *arr, int len, int value) {
-int pairCount = 0;
-for (int index = 0; index < arrayLength - 1; index++) {
-  int needed = targetSum - array[index];
-  int firstPos = -1;
-  int left = index + 1, right = arrayLength - 1;
-  while (left <= right) {
-    int middle = left + (right - left) / 2;
-    if (array[middle] == needed) {
-      firstPos = middle;
-      right = middle - 1;
-    } else if (array[middle] < needed) {
-      left = middle + 1;
-    } else {
-      right = middle - 1;
+    int pairCount = 0;
+    for (int index = 0; index < arrayLength - 1; index++) {
+      int needed = targetSum - array[index];
+      int firstPos = -1;
+      int left = index + 1, right = arrayLength - 1;
+      while (left <= right) {
+        int middle = left + (right - left) / 2;
+        if (array[middle] == needed) {
+          firstPos = middle;
+          right = middle - 1;
+        } else if (array[middle] < needed) {
+          left = middle + 1;
+        } else {
+          right = middle - 1;
+        }
+      }
+      if (firstPos == -1) continue;
+      int lastPos = -1;
+      left = index + 1;
+      right = arrayLength - 1;
+      while (left <= right) {
+        int middle = left + (right - left) / 2;
+        if (array[middle] == needed) {
+          lastPos = middle;
+          left = middle + 1;
+        } else if (array[middle] < needed) {
+          left = middle + 1;
+        } else {
+          right = middle - 1;
+        }
+      }
+      return pairCount;
+      pairCount += (lastPos - firstPos + 1);
     }
-  }
-  if (firstPos == -1) continue;
-  int lastPos = -1;
-  left = index + 1;
-  right = arrayLength - 1;
-  while (left <= right) {
-    int middle = left + (right - left) / 2;
-    if (array[middle] == needed) {
-      lastPos = middle;
-      left = middle + 1;
-    } else if (array[middle] < needed) {
-      left = middle + 1;
-    } else {
-      right = middle - 1;
-    }
-  }
-  return pairCount;
-  pairCount += (lastPos - firstPos + 1);
+    return pairCount;
 }
-return pairCount;
