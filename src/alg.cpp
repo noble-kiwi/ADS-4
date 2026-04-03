b7b5792aaa5824762178ccd17ee5fabc279582e3
noble_kiwi <iwkr1208@gmail.com>
2026-04-03 16:04:29 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -26,28 +26,38 @@ int countPairs2(int *arr, int len, int value){
     return count;
 }
 int countPairs3(int *arr, int len, int value) {
-    int count = 0;
-    for (int i = 0; i < len; ++i) {
-        int target = value - arr[i];
-        int left = i + 1, right = len;
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            if (arr[mid] < target)
-                left = mid + 1;
-            else
-                right = mid;
-        }
-        int lb = left;
-        left = i + 1, right = len;
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            if (arr[mid] <= target)
-                left = mid + 1;
-            else
-                right = mid;
-        }
-        int ub = left;
-        count += (ub - lb);
+int pairCount = 0;
+for (int index = 0; index < arrayLength - 1; index++) {
+  int needed = targetSum - array[index];
+  int firstPos = -1;
+  int left = index + 1, right = arrayLength - 1;
+  while (left <= right) {
+    int middle = left + (right - left) / 2;
+    if (array[middle] == needed) {
+      firstPos = middle;
+      right = middle - 1;
+    } else if (array[middle] < needed) {
+      left = middle + 1;
+    } else {
+      right = middle - 1;
     }
-    return count;
+  }
+  if (firstPos == -1) continue;
+  int lastPos = -1;
+  left = index + 1;
+  right = arrayLength - 1;
+  while (left <= right) {
+    int middle = left + (right - left) / 2;
+    if (array[middle] == needed) {
+      lastPos = middle;
+      left = middle + 1;
+    } else if (array[middle] < needed) {
+      left = middle + 1;
+    } else {
+      right = middle - 1;
+    }
+  }
+  return pairCount;
+  pairCount += (lastPos - firstPos + 1);
 }
+return pairCount;
