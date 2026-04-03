6fb5351fd01bb419d4cc26809466ae8363a4b1d5
noble_kiwi <iwkr1208@gmail.com>
2026-04-03 16:06:47 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -11,7 +11,7 @@ int countPairs1(int *arr, int len, int value) {
     return count;
 }
 
-int countPairs2(int *arr, int len, int value){
+int countPairs2(int *arr, int len, int value) {
     int count = 0;
     for (int i = 0; i < len; ++i) {
         for (int j = len - 1; j > i; --j) {
